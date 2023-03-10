43408931c25ae1722830f48aa4082a4243ddefb4
Dominik Chally <dominik.chally@yahoo.com>
2023-03-10 00:16:06 -0600

    Added index.js and data.json files. Also edited html in humdity and temperature to access json

---
+++ index.js
@@ -0,0 +1,37 @@
+
+
+
+function updateTemp() {
+	console.log("YO TESING ONLOAD");
+	temp = document.getElementById('temp');
+	
+	
+	fetch('data.json')
+		.then(function (response) {
+			return response.json();
+		})
+		.then(function (data) {
+			temp.innerHTML = data.temp;
+		})
+		.catch(function (err) {
+			console.log(err);
+		});
+	setTimeout(updateTemp, 5000);
+}
+
+function updateHumidity() {
+	humidity = document.getElementById('humidity');
+
+	fetch('data.json')
+		.then(function (response) {
+			return response.json();
+		})
+		.then(function (data) {
+			humidity.innerHTML = data.temp;
+		})
+		.catch(function (err) {
+			console.log(err);
+		});
+	setTimeout(updateHumidity, 5000);
+}
+
